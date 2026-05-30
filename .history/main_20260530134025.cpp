f2791a56c319c360d6e8ca4165b43e149090a45d
Wang HongYong <why17573315302@qq.com>
2026-05-30 13:49:12 +0800

    four

--- .history/main_20260530134025.cpp
+++ .history/main_20260530134025.cpp
@@ -83,70 +83,84 @@ switch(choice){
     }
     case 4:{
         cout<<"请输入新景点的编号、名称和描述：";
-        int id; string name, desc;
+        int id;
+        string name, desc;
         cin>>id>>name>>desc;
-        if(addScene(g,id,name,desc))
+        if(addScene(g,id,name,desc)){
             cout<<"景点添加成功！"<<endl;
-        else
+            saveScenes(g,"data/scene.txt");
+        } else {
             cout<<"景点添加失败！可能是编号已存在。"<<endl;
+        }
         break;
     }
     case 5:{
         cout<<"请输入要删除的景点编号：";
-        int id; cin>>id;
-        if(deleteScene(g,id))
+        int id;
+        cin>>id;
+        if(deleteScene(g,id)){
             cout<<"景点删除成功！"<<endl;
-        else
-            cout<<"景点删除失败！可能是编号不存在。"<<endl;
-        break;
-    }
-    case 6:{
-        cout<<"请输入要修改的景点编号：";
-        int id; cin>>id;
-        int index=getIdIndex(g.scenes,id);
-        if(index==-1){
-            cout<<"景点编号不存在！"<<endl;
-        }else{
-            cout<<"请输入新的名称和描述：";
-            string name,desc; cin>>name>>desc;
-            g.scenes[index].name=name;
-            g.scenes[index].description=desc;
             saveScenes(g,"data/scene.txt");
-            cout<<"景点修改成功！"<<endl;
+            saveRoads(g,"data/road.txt");//删除景点后相关道路也要更新
+        } else {
+            cout<<"景点删除失败！可能是编号不存在。"<<endl;
         }
         break;
     }
-    case 7:{
-        cout<<"请输入道路的起点编号、终点编号、权重和类型(0步行,1车行)：";
-        int from,to,weight,type; cin>>from>>to>>weight>>type;
-        if(addRoad(g,from,to,weight,type))
+    case 6:
+    {
+        cout<<"请输入要修改的景点编号：";
+        int id;
+        string name, desc;
+        cin>>id;
+        cout<<"请输入新的名称和描述：";
+        cin>>name>>desc;
+        if(updateScene(g,id,name,desc)){
+            cout<<"景点修改成功！"<<endl;
+            saveScenes(g,"data/scene.txt");
+        } else {
+            cout<<"景点修改失败！可能是编号不存在。"<<endl;
+        }
+        break;
+    }
+    case 7:
+    {
+        cout<<"请输入道路的起点编号、终点编号、权重和道路类型(0步行道,1车行道)：";
+        int from, to, weight, type;
+        cin>>from>>to>>weight>>type;
+        if(addRoad(g,from,to,weight,type)){
             cout<<"道路添加成功！"<<endl;
-        else
-            cout<<"道路添加失败！"<<endl;
-        break;
-    }
-    case 8:{
-        cout<<"请输入要删除道路的起点和终点编号：";
-        int from,to; cin>>from>>to;
-        if(deleteRoad(g,from,to))
-            cout<<"道路删除成功！"<<endl;
-        else
-            cout<<"道路删除失败！"<<endl;
-        break;
-    }
-    case 9:{
-        cout<<"请输入要修改道路的起点和终点编号：";
-        int from,to; cin>>from>>to;
-        int fi=getIdIndex(g.scenes,from),ti=getIdIndex(g.scenes,to);
-        if(fi==-1||ti==-1){
-            cout<<"编号不存在！"<<endl;
-        }else{
-            cout<<"请输入新的权重和类型(0步行,1车行)：";
-            int weight,type; cin>>weight>>type;
-            g.scenes[fi].edges[ti]={to,weight,type};
-            g.scenes[ti].edges[fi]={from,weight,type};
             saveRoads(g,"data/road.txt");
+        } else {
+            cout<<"道路添加失败！可能是起点或终点编号不存在，或者道路已存在。"<<endl;
+        }
+        break;
+    }
+    case 8:
+    {
+        cout<<"请输入要删除的道路的起点编号和终点编号：";
+        int from, to;
+        cin>>from>>to;
+        if(deleteRoad(g,from,to)){
+            cout<<"道路删除成功！"<<endl;
+            saveRoads(g,"data/road.txt");
+        } else {
+            cout<<"道路删除失败！可能是起点或终点编号不存在，或者道路不存在。"<<endl;
+        }
+        break;
+    }
+    case 9:
+    {
+        cout<<"请输入要修改的道路的起点编号和终点编号：";
+        int from, to, weight, type;
+        cin>>from>>to;
+        cout<<"请输入新的权重和道路类型(0步行道,1车行道)：";
+        cin>>weight>>type;
+        if(updateRoad(g,from,to,weight,type)){
             cout<<"道路修改成功！"<<endl;
+            saveRoads(g,"data/road.txt");
+        } else {
+            cout<<"道路修改失败！可能是起点或终点编号不存在，或者道路不存在。"<<endl;
         }
         break;
     }
